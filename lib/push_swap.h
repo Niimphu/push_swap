56a452fdabe53e27069f7fde72a24a2d1c3cf911
yiwong <yiwong@student.42wolfsburg.de>
2023-02-24 15:55:17 +0100

    libUpdate

--- lib/push_swap.h
+++ lib/push_swap.h
@@ -6,11 +6,13 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:03:56 by yiwong            #+#    #+#             */
-/*   Updated: 2023/02/24 14:07:39 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/02/24 15:54:02 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include "libft.h"
+#include "ft_printf.h"
 #include <stdlib.h>
 
