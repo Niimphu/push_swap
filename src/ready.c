b67eb40cd5d35eb3f036ff43d1ac1c92ffaa336d
yiwong <yiwong@student.42wolfsburg.de>
2023-02-25 02:37:18 +0100

    late

--- src/ready.c
+++ src/ready.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:24:37 by yiwong            #+#    #+#             */
-/*   Updated: 2023/02/24 15:12:04 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/02/24 23:28:06 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ int	create_list()
 {
 	int		i;
 	t_list	stack_a;
+	t_list	new;
 
 	if (argc <= 1)
 		exit(0);
 	stack_a = ft_lstnew(argv[1]);
-	if (!stack_a)
+	if (!stack_a || stack_a->current)
 		exit(0);
 	while (i <= argc)
 	{
-		
+		ft_lstadd_back(stack_a, ft_lstnew(argv[i++]));
 	}
 }
 
