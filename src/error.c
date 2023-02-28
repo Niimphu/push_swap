4c81b4cdd6660d2545f2331e4d92b80c95fcb9f0
yiwong <yiwong@student.42wolfsburg.de>
2023-02-28 17:50:51 +0100

    errorProgress

--- src/error.c
+++ src/error.c
@@ -6,11 +6,33 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 15:54:05 by yiwong            #+#    #+#             */
-/*   Updated: 2023/02/24 16:38:14 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/02/28 17:40:41 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	validate()
+
+int	repeat_check(t_list lst)
+{
+	t_list	last;
+
+	last = ft_lstlast(lst);
+	while (lst)
+	{
+		if (lst -> content == last -> content && lst -> next)
+			return (1);
+		lst = lst -> next;
+	}
+	return (0);
+}
+
+int	error_check(t_list stack_a)
+{
+	repeat_check(stack_a);
+	ft_lstiter(stack_a, validate);
+}
 
 int	error_exit(void)
 {
--- src/ready.c
+++ src/ready.c
@@ -6,30 +6,33 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:24:37 by yiwong            #+#    #+#             */
-/*   Updated: 2023/02/24 23:28:06 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/02/28 17:50:29 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	create_list()
+int	create_list(int argc, char *argv[])
 {
 	int		i;
 	t_list	stack_a;
 	t_list	new;
 
-	if (argc <= 1)
-		exit(0);
 	stack_a = ft_lstnew(argv[1]);
-	if (!stack_a || stack_a->current)
+	if (!stack_a)
 		exit(0);
 	while (i <= argc)
 	{
 		ft_lstadd_back(stack_a, ft_lstnew(argv[i++]));
+		error_check(stack_a);
 	}
+	return (0);
 }
 
 int	init(int argc, char *argv[])
 {
+	if (argc <= 1)
+		exit(0);
 	create_list(argc - 1, &argv[1]);
+	return (0);
 }
