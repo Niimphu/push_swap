42804f9bb5e5ca2ff193c0a114b3b3e937095d1c
yiwong <yiwong@student.42wolfsburg.de>
2023-03-09 17:17:18 +0100

    includeFixed

--- lib/push_swap.h
+++ lib/push_swap.h
@@ -6,15 +6,15 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 14:03:56 by yiwong            #+#    #+#             */
-/*   Updated: 2023/03/07 16:50:48 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/03/09 17:11:57 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "libft.h"
-# include "ft_printf.h"
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
 # include <stdlib.h>
 
 int	init(int argc, char *argv[]);
