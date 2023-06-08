a31391b8dbea62e4052f5ebf1e82827e83ccc353
Niimphu <nimphu@outlook.com>
2023-06-08 13:45:22 +0200

    fixed leak in cases of invalid input

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 17:42:16 by yiwong            #+#    #+#             */
-/*   Updated: 2023/05/22 16:55:05 by yiwong           ###   ########.fr       */
+/*   Updated: 2023/06/08 13:44:01 by yiwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ int	main(int argc, char *argv[])
 		return (1);
 	set->stack_a = init_stack_a(argc, argv);
 	if (!set->stack_a)
+	{
+		free(set);
 		return (1);
+	}
 	push_swap(set);
 	stack_clear(set->stack_a);
 	free(set);
